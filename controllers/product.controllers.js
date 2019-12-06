e014f6adf2e9352a70bfaea3d0da250ec55d6e8c
nguyenhongthaj <nguyenhongthajii@gmail.com>
2019-12-06 08:13:46 +0700

    'pagination'

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -2,8 +2,12 @@ var Product = require('../models/product.model');
 
 module.exports.index = async function(req,res){
     var product = await Product.find();
+    var page = parseInt(req.query.page || 1); //n -- so trang
+    var perPage = 4; //x - so Items hien thi tren 1 trang
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
 	res.render('products/index',{
-		Product: product.slice(0,4)
+		Product: product.slice(start,end)
 		});	
 }
 module.exports.create = function(req,res){
